fd34a14eb64027799768e99edb7084528ba932e0
akhilesh3721 <akhilesh.s3721@gmail.com>
2026-02-18 18:55:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Add smooth scrolling for navigation links
-    const links = document.querySelectorAll('a[href^="#"]');
-
-    links.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const target = document.querySelector(link.getAttribute('href'));
-            window.scrollTo({
-                top: target.offsetTop - 50, // Adjust the scroll position
-                behavior: 'smooth',
-            });
-        });
-    });
-});
